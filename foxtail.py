31a95b8b7fd9695de62bb9f047c6bc444f6c7362
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-06-08 12:22:33 -0900

    Update foxtail.py

--- foxtail.py
+++ foxtail.py
@@ -12,43 +12,34 @@ while running:
             os.chdir(path + "/" + change)
         except:
             print("File is either not a directory or dose not exist.")
+    elif inp == "l":
+        os.system("ls")
+    elif inp == "cwd":
+        print(path)
+    elif inp.startswith("echo ") == True:
+        out = inp.replace("echo ", "")
+        print(out)
+    elif inp.startswith("mdir ") == True:
+        mk = inp.replace("mdir ", "")
+        if not os.system("mkdir " + mk) == 0:
+            print("bash error")
+    elif inp.startswith("rem ") == True:
+        rm = inp.replace("rem ", "")
+        if not os.system("rm " + rm) == 0:
+            print("bash error")
+    elif inp.startswith("remdir ") == True:
+        rm = inp.replace("remdir ", "")
+        if not os.system("rm -r " + rm) == 0:
+            print("bash error")
+    elif inp.startswith("mf ") == True:
+        add = inp.replace("mf ", "")
+        if not os.system("touch " + add) == 0:
+            print("bash error")
+    elif inp.startswith("read ") == True:
+        read = inp.replace("read ", "")
+        if not os.system("cat " + read) == 0:
+            print("bash error")
+    elif inp == "exit":
+        running = False
     else:
-        if inp == "l":
-            os.system("ls")
-        else:
-            if inp == "cwd":
-                print(path)
-            else:
-                if inp.startswith("echo ") == True:
-                    out = inp.replace("echo ", "")
-                    print(out)
-                else:
-                    if inp.startswith("mdir ") == True:
-                        mk = inp.replace("mdir ", "")
-                        if not os.system("mkdir " + mk) == 0:
-                            print("bash error")
-                    else:
-                        if inp.startswith("rem ") == True:
-                            rm = inp.replace("rem ", "")
-                            if not os.system("rm " + rm) == 0:
-                                print("bash error")
-                        else:
-                            if inp.startswith("remdir ") == True:
-                                rm = inp.replace("remdir ", "")
-                                if not os.system("rm -r " + rm) == 0:
-                                    print("bash error")
-                            else:
-                                if inp.startswith("mf ") == True:
-                                    add = inp.replace("mf ", "")
-                                    if not os.system("touch " + add) == 0:
-                                        print("bash error")
-                                else:
-                                    if inp.startswith("read ") == True:
-                                        read = inp.replace("read ", "")
-                                        if not os.system("cat " + read) == 0:
-                                            print("bash error")
-                                    else:
-                                        if inp == "exit":
-                                            running = False
-                                        else:
-                                            NU()
+        NU()
