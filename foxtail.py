00a41bcfff12181ebc2ace1fdfc3fb0856ea888a
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-06-16 08:43:57 -0400

    get rid of extra lines

--- foxtail.py
+++ foxtail.py
@@ -8,7 +8,7 @@ while running:
     path_ = os.getcwd()
     path = format(path_)
     inp = input(c(path, 'blue') + c(" $", 'green'))
-    if inp.startswith("cd ") == True:
+    if inp.startswith("cd "):
         try:
             change = inp.replace("cd ", "")
             os.chdir(path + "/" + change)
@@ -18,26 +18,26 @@ while running:
         os.system("ls")
     elif inp == "cwd":
         print(c(path, 'green'))
-    elif inp.startswith("echo ") == True:
+    elif inp.startswith("echo "):
         out = inp.replace("echo ", "")
         print(out)
-    elif inp.startswith("mdir ") == True:
+    elif inp.startswith("mdir "):
         mk = inp.replace("mdir ", "")
         if not os.system("mkdir " + mk) == 0:
             print("bash error")
-    elif inp.startswith("rem ") == True:
+    elif inp.startswith("rem "):
         rm = inp.replace("rem ", "")
         if not os.system("rm " + rm) == 0:
             print("bash error")
-    elif inp.startswith("remdir ") == True:
+    elif inp.startswith("remdir "):
         rm = inp.replace("remdir ", "")
         if not os.system("rm -r " + rm) == 0:
             print("bash error")
-    elif inp.startswith("mf ") == True:
+    elif inp.startswith("mf "):
         add = inp.replace("mf ", "")
         if not os.system("touch " + add) == 0:
             print("bash error")
-    elif inp.startswith("read ") == True:
+    elif inp.startswith("read "):
         read = inp.replace("read ", "")
         if not os.system("cat " + read) == 0:
             print("bash error")
